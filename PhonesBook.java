5c07233d635a4f9273d7208d4cc683b07d5cbd29
Игорь Демьяненко <cmp.int0x80@gmail.com>
2023-10-12 02:10:08 +0300

    1

---
+++ PhonesBook.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhonesBook {
+    public static void main(String[] args) {
+        System.out.println("Введите имя и номер телефона через пробел.\n" +
+                            "Если номеров несколько, введите их через запятую.\n" +
+                            "Для выхода введите 'q'.");
+        HashMap<String, String> phonesBase = new HashMap<String, String>();
+        Scanner inputStream = new Scanner(System.in);
+        String userInput = "";
+        
+        while (true) {
+            userInput = inputStream.nextLine();
+            userInput = userInput.substring(0, 1).toUpperCase() +
+                            userInput.substring(1).toLowerCase();
+            if (userInput.split(userInput).length != 2) {
+                if (userInput.equalsIgnoreCase("q")){ break; }
+                System.out.println("Некорректные данные, повторите попытку.");
+            }
+        }
+
+        inputStream.close();
+    }
+}
