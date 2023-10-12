3de93cbc8c3fd9e246f2c3db83a3066601470a0f
Игорь Демьяненко <cmp.int0x80@gmail.com>
2023-10-12 03:57:26 +0300

    beta

--- PhonesBook.java
+++ PhonesBook.java
@@ -1,9 +1,9 @@
 import java.util.HashMap;
-import java.util.List;
+// import java.util.List;
 import java.util.Scanner;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;;
+// import java.lang.reflect.Array;
+// import java.util.ArrayList;
+// import java.util.Collections;;
 
 public class PhonesBook {
 
@@ -17,10 +17,10 @@ public class PhonesBook {
     // }
 
     public static void main(String[] args) {
-        System.out.println("Введите имя и номер телефона через пробел.\n" +
-                            "Если номеров несколько, введите их через запятую.\n" +
+        System.out.println("Введите имя и номер телефона через двоеточие.\n" +
+                            // "Если номеров несколько, введите их через запятую.\n" +
                             "Для выхода введите 'q'.");
-        HashMap<String, String[]> phonesBase = new HashMap<String, String[]>();
+        HashMap<String, String> phonesBase = new HashMap<String, String>();
         Scanner inputStream = new Scanner(System.in);
         String userInput = "";
         
@@ -28,25 +28,27 @@ public class PhonesBook {
             userInput = inputStream.nextLine();
             userInput = userInput.substring(0, 1).toUpperCase() +
                             userInput.substring(1).toLowerCase();
-            if (userInput.split(" ").length != 2) {
+            if (userInput.split(":").length != 2) {
                 if (userInput.equalsIgnoreCase("q")){ break; }
                 System.out.println("Некорректные данные, повторите попытку.");
             }
 
-            String contactName = userInput.split(" ")[0];
-            String[] contactPhones = userInput.split(" ")[1].split(",");
+            String contactName = userInput.split(":")[0];
+            String contactPhone = userInput.split(":")[1];
 
             if (phonesBase.containsKey(contactName)) {
-                String[] resPhones = MergeArrays(phonesBase.get(contactName), contactPhones);
+                // String[] resPhones = MergeArrays(phonesBase.get(contactName), contactPhones);
+                String resPhones = phonesBase.get(contactName) + ", " + contactPhone;
                 phonesBase.put(contactName, resPhones);
+                System.out.println("Номер успешно добавлен к существующему контакту.");
             } else {
-                phonesBase.put(contactName, contactPhones);
+                phonesBase.put(contactName, contactPhone);
+                System.out.println("Контакт успешно добавлен.");
             }
         }
 
         for (String i : phonesBase.keySet()) {
-            System.out.println(i + ": " + phonesBase.get(i).toString());
-            System.out.println(i + ": " + phonesBase.get(i).toString());
+            System.out.println(i + ": " + phonesBase.get(i));
         }
 
         inputStream.close();
