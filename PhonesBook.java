29033009b9d77d85def1f15537e408fb9e15b0cd
Игорь Демьяненко <cmp.int0x80@gmail.com>
2023-10-12 03:40:40 +0300

    Multi nombers to single

--- PhonesBook.java
+++ PhonesBook.java
@@ -1,12 +1,26 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;;
 
 public class PhonesBook {
+
+    // private static <T> T[] MergeArrays(T[] firstArray, T[] secondArray) {
+    //     List<T> resArray = new ArrayList<>(firstArray.length + secondArray.length);
+    //     Collections.addAll(resArray, firstArray);
+    //     Collections.addAll(resArray, secondArray);
+    //     @SuppressWarnings("unchecked")
+    //     T[] typeOfArray = (T[]) Array.newInstance(firstArray.getClass().componentType(), 0);
+    //     return resArray.toArray(typeOfArray);
+    // }
+
     public static void main(String[] args) {
         System.out.println("Введите имя и номер телефона через пробел.\n" +
                             "Если номеров несколько, введите их через запятую.\n" +
                             "Для выхода введите 'q'.");
-        HashMap<String, String> phonesBase = new HashMap<String, String>();
+        HashMap<String, String[]> phonesBase = new HashMap<String, String[]>();
         Scanner inputStream = new Scanner(System.in);
         String userInput = "";
         
@@ -19,6 +33,20 @@ public class PhonesBook {
                 System.out.println("Некорректные данные, повторите попытку.");
             }
 
+            String contactName = userInput.split(" ")[0];
+            String[] contactPhones = userInput.split(" ")[1].split(",");
+
+            if (phonesBase.containsKey(contactName)) {
+                String[] resPhones = MergeArrays(phonesBase.get(contactName), contactPhones);
+                phonesBase.put(contactName, resPhones);
+            } else {
+                phonesBase.put(contactName, contactPhones);
+            }
+        }
+
+        for (String i : phonesBase.keySet()) {
+            System.out.println(i + ": " + phonesBase.get(i).toString());
+            System.out.println(i + ": " + phonesBase.get(i).toString());
         }
 
         inputStream.close();
